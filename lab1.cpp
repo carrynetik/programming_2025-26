ad3a913bb83e8cee4d100b4825e316e0f3b0da72
Alexander Postrigan <128978693+carrynetik@users.noreply.github.com>
2025-10-10 02:18:12 +0200

    Update lab1.cpp

--- lab1.cpp
+++ lab1.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <cmath>
+#include <bitset>
 
 int main() {
     int r;
@@ -8,8 +8,10 @@ int main() {
     std::cin >> r;
     std::cout << "Тип: float" << std::endl; //выбрал float, тк число с плавающей запятой(не целое)
     std::cout << "Размер в байтах: " << sizeof(konstanta) << std::endl;
-    float minsize = pow(2, -126);
-    float maxsize = (2 - pow(2, -23)) * pow(2, 127);
+    // float minsize = pow(2, -126);
+    // float maxsize = (2 - pow(2, -23)) * pow(2, 127);   изначально думал так, но потом увидел что есть int_max, почему то на маке надо с двух сторон нижнее подчеркивание
+    float maxsize = __FLT_MAX__;
+    float minsize = __FLT_MIN__;
     std::cout << "Минимальное значениe: " << minsize << std::endl;
     std::cout << "Максимальное значение: " << maxsize << std::endl;
     std::cout << "V = " << (4 * konstanta * pow(r, 3)) / 3 << std::endl;
